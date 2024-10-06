55b463fea4818cb431f92c19f956cc4335c1d670
Gizemdemirz <gizem.demrz@gmail.com>
2024-10-06 22:04:11 +0300

    .

--- CsharpEgitimi/Properties/AssemblyInfo.cs
+++ CsharpEgitimi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CsharpEgitimi")]
+[assembly: AssemblyTitle("01_MainSubjects")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CsharpEgitimi")]
+[assembly: AssemblyProduct("01_MainSubjects")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
